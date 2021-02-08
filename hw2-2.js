a8282348b34ebe33c51a79d0e1073ffd79b9e8f9
p22crsullivan <p22crsullivan@fairfieldprep.org>
2021-02-08 13:50:20 -0500

    HW 2-2 Submission

---
+++ hw2-2.js
@@ -0,0 +1,12 @@
+var myName = "Christopher Sullivan";
+var date = "02/08/2021";
+var attack = 20;
+var defense = 20;
+var enemyCount = 3;
+var warriorAttack = attack - 2;
+var warriorDefense = defense * 2;
+var rangerAttack = attack * 3;
+var rangerDefense = defense - 1;
+var mageAttack = enemyCount ** attack;
+var mageDefense = defense / enemyCount;
+"The warrior's attack is :## and his defense is :##."
